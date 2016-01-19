6229e5d679c60ec9774a7354b3fc63dfcf450cbf
Daniel Pape <danielwpape@gmail.com>
2016-01-19 21:35:28 +0000

    Initial Commit

---
+++ eve4/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  eve4
+//
+//  Created by Daniel Pape on 19/01/2016.
+//  Copyright © 2016 Daniel Pape. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
